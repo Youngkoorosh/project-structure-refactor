fb2061def219f8e48af770eb7f09e4a99e96af14
koorosh <Koorosh.nrp@gmail.com>
2025-09-01 19:59:58 +0330

    first version

---
+++ main.sh
@@ -0,0 +1,3 @@
+mv project/devv project/dev && mv project/dev/codes/code1.py project/dev/codes/main.py && mv project/dev/codes/code2.py project/dev/codes/utils.py
+rm -r project/dev/logs
+cp -r project/tools project/dev/
